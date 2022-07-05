992f38d1c6d54fcb09f335a1800906049ea843c5
unknown <nduflot@dawan.fr>
2022-07-05 15:02:08 +0200

    js storage pt 1: sessionStorage sp 1

---
+++ js/utilities.js
@@ -0,0 +1,20 @@
+/* attente du chargement de la page */
+function loaded(callable){
+    window.addEventListener('DOMContentLoaded', callable);
+}
+
+/* fonction raccourcie du querySelector */
+function qS(selector){
+    return document.querySelector(selector);
+}
+
+/* fonction raccourcie du querySelectorAll */
+function qSAll(selector){
+    return document.querySelectorAll(selector);
+}
+
+/* création d'une fonction d'initialisation / destruction de cookie */
+
+
+/* récupération d'un cookie */
+
