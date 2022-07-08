a6c240913dc81ed4858c9c961dfad58100ca55a5
unknown <nduflot@dawan.fr>
2022-07-08 16:31:24 +0200

    js POO les class et class étendues

---
+++ js/class/aventure/Voleur.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Classe Voleur, étendue d'Aventurier
+ * 
+ * changer le coup spécial
+ * sneak attack ou attaque discrète (base 2d6 dg)
+ * 
+ * coup spécial = dégat de base + dégat supplémentaire de sneakAttack
+ * 2d à 6f
+ * 
+ * le voleur est équipé de base d'une dague niveau de dégât 1.5
+ * 
+ */
+
+/* */
